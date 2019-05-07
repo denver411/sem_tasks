a21d6892ed209d877bb27cd7c93bfda2c870ba71
Denis Zhebov <zhebov@gmail.com>
2019-05-07 08:37:30 +0300

    Зачатки быстрой сортировки

--- 8. mergeSort & quickSort.js
+++ 8. mergeSort & quickSort.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// сортировка слиянием
 const mergeSort = arr => {
   if (arr.length <= 1) return arr;
-  
+
   const f = ([x1, ...xs1], [x2, ...xs2], acc) => {
     if (x1 == null) return [...acc, x2, ...xs2];
     if (x2 == null) return [...acc, x1, ...xs1];
@@ -11,23 +12,31 @@ const mergeSort = arr => {
       ? f(xs1, [x2, ...xs2], [...acc, x1])
       : f([x1, ...xs1], xs2, [...acc, x2]);
   };
-
   const middle = Math.floor(arr.length) / 2;
 
   return f(mergeSort(arr.slice(0, middle)), mergeSort(arr.slice(middle)), []);
 };
 
+// быстрая сортировка
+const quickSort = ([x, ...xs]) => {
+  if (x == null) return [];
+  const less = xs.filter(el => el <= x);
+  const more = xs.filter(el => el > x);
+
+  return [...quickSort(less), x, ...quickSort(more)];
+};
+
 //тесты
 const generateArr = (length = 10, max = 10) => {
-    const arr = [];
-  
-    for (let i = 0; i < length; i++) {
-      arr.push(Math.floor(Math.random() * max));
-    }
-  
-    return arr;
-  };
-  
-  const arr = generateArr();
-  console.log(arr);
-  console.log(mergeSort(arr));
+  const arr = [];
+
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.floor(Math.random() * max));
+  }
+
+  return arr;
+};
+
+const arr = generateArr(10, 100);
+console.log(arr);
+console.log(quickSort(arr));
